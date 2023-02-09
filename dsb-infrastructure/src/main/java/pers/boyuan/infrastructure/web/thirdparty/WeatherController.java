f45149238f2601be10277a1ae6e747f3fcf59be5
ZhangBoyuan <byuan98@outlook.com>
2023-02-09 15:43:21 +0800

    feat: 天气查询原服务方停止服务，相关接口暂时废弃

--- dsb-infrastructure/src/main/java/pers/boyuan/infrastructure/web/thirdparty/WeatherController.java
+++ dsb-infrastructure/src/main/java/pers/boyuan/infrastructure/web/thirdparty/WeatherController.java
@@ -1,21 +1,10 @@
 package pers.boyuan.infrastructure.web.thirdparty;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import pers.boyuan.common.constants.ResponseEnum;
-import pers.boyuan.common.dto.Response;
-import pers.boyuan.common.util.StringUnzipUtil;
 import pers.boyuan.thirdparty.weather.WeatherService;
-
-import java.io.IOException;
-import java.util.Objects;
 
 /**
  * 天气相关接口 前端控制器
@@ -23,6 +12,7 @@ import java.util.Objects;
  * @author ZhangBoyuan
  * @date 2022-06-14
  */
+@Deprecated
 @RestController
 @RequestMapping("/thirdparty/weather")
 @Api(tags = "天气相关接口")
@@ -51,19 +41,22 @@ public class WeatherController {
      */
     private final String RESPONSE_FORECAST_KEY = "forecast";
 
-    @GetMapping("/query")
-    @ApiOperation("根据城市名称查询天气")
-    public Response query(@RequestParam(name = "city") String cityName) throws IOException {
-        String unzipStr = StringUnzipUtil.gzip(weatherService.getCityWeather(cityName));
-
-        if (Objects.nonNull(unzipStr)) {
-            JsonNode jsonNode = new ObjectMapper().readValue(unzipStr, JsonNode.class);
-            if (RESPONSE_SUCCESS_CODE.equals(jsonNode.get(RESPONSE_STATUS_KEY).asText())) {
-                return Response.success(jsonNode.get(RESPONSE_DATA_KEY).get(RESPONSE_FORECAST_KEY));
-            }
-        }
-
-        return Response.error(ResponseEnum.FAIL);
-    }
+    /**
+     * 原服务方停止服务，该接口废弃
+     */
+//    @GetMapping("/query")
+//    @ApiOperation("根据城市名称查询天气")
+//    public Response query(@RequestParam(name = "city") String cityName) throws IOException {
+//        String unzipStr = StringUnzipUtil.gzip(weatherService.getCityWeather(cityName));
+//
+//        if (Objects.nonNull(unzipStr)) {
+//            JsonNode jsonNode = new ObjectMapper().readValue(unzipStr, JsonNode.class);
+//            if (RESPONSE_SUCCESS_CODE.equals(jsonNode.get(RESPONSE_STATUS_KEY).asText())) {
+//                return Response.success(jsonNode.get(RESPONSE_DATA_KEY).get(RESPONSE_FORECAST_KEY));
+//            }
+//        }
+//
+//        return Response.error(ResponseEnum.FAIL);
+//    }
 
 }
