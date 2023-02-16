b810319caa94aa6132a4bbc282900debc62de4ec
ZhangBoyuan <byuan98@outlook.com>
2023-02-16 16:28:45 +0800

    feat: 新增常用工具接口，添加编程常用工具-静态变量名生成

---
+++ dsb-application/src/main/java/pers/boyuan/application/tools/impl/ToolsAppServiceImpl.java
@@ -0,0 +1,19 @@
+package pers.boyuan.application.tools.impl;
+
+/**
+ * 常用工具汇总应用服务
+ *
+ * @author ZhangBoyuan
+ * @date 2023-02-16
+ */
+public interface ToolsAppServiceImpl {
+
+    /**
+     * 静态变量名生成
+     *
+     * @param str 待处理数据
+     * @return 处理后数据
+     */
+    String staticVariableNameVariable(String str);
+
+}
