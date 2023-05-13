ce651fc7f6c9d694a48d8d359fdeedcb7cbb38d7
ZhangBoyuan <byuan98@outlook.com>
2023-05-13 15:27:12 +0800

    refactor: 代码规范性优化

--- dsb-boot/src/main/java/pers/boyuan/GlobalExceptionHandler.java
+++ dsb-boot/src/main/java/pers/boyuan/GlobalExceptionHandler.java
@@ -9,11 +9,13 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import pers.boyuan.common.constants.ResponseEnum;
 import pers.boyuan.common.dto.Response;
 import pers.boyuan.common.exception.CustomException;
 
 import java.util.List;
+
+import static pers.boyuan.common.constants.ResponseEnum.EXCEPTION;
+import static pers.boyuan.common.constants.ResponseEnum.PARAM_NOT_STANDARD;
 
 /**
  * 全局异常捕获
@@ -27,15 +29,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     public Response handle(Exception e) {
         if (e instanceof HttpRequestMethodNotSupportedException) {
-            return Response.error(ResponseEnum.EXCEPTION.getCode(), e.getMessage());
+            return Response.error(EXCEPTION.getCode(), e.getMessage());
         }
         log.error("系统错误：", e);
-        return Response.error(ResponseEnum.EXCEPTION);
+        return Response.error(EXCEPTION);
     }
 
     @ExceptionHandler(value = CustomException.class)
     public Response handle(CustomException e) {
-        log.error("业务异常：{}", e);
+        log.error("业务异常：{}", e.getMessage());
         return Response.error(e.getCode(), e.getMessage());
     }
 
@@ -59,11 +61,11 @@ public class GlobalExceptionHandler {
                 ObjectError objectError = allErrors.get(0);
                 String defaultMessage = objectError.getDefaultMessage();
                 log.error("入参不规范：{}", defaultMessage);
-                return Response.error(ResponseEnum.PARAM_NOT_STANDARD.getCode(), objectError.getDefaultMessage());
+                return Response.error(PARAM_NOT_STANDARD.getCode(), objectError.getDefaultMessage());
             }
         }
         log.error("未知入参异常");
-        return Response.error(ResponseEnum.EXCEPTION);
+        return Response.error(EXCEPTION);
     }
 
 }
