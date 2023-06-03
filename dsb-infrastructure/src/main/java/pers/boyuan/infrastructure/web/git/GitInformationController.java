7d9a18fdbcd8ae0f2186a850f6252f639526b080
ZhangBoyuan <byuan98@outlook.com>
2023-06-03 16:24:10 +0800

    feat: Optimizing the Git Information Retrieval Code

--- dsb-infrastructure/src/main/java/pers/boyuan/infrastructure/web/git/GitInformationController.java
+++ dsb-infrastructure/src/main/java/pers/boyuan/infrastructure/web/git/GitInformationController.java
@@ -12,6 +12,7 @@ import pers.boyuan.common.dto.Response;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Properties;
 
 /**
@@ -28,33 +29,58 @@ public class GitInformationController {
 
     @Data
     class GitDetail {
-        private String branch;
-
-        private String commitMessage;
-
-        private String commitTime;
+        String branch;
+        String commitMessage;
+        String commitTime;
     }
 
     @ApiOperation("获取本次打包git信息")
     @GetMapping("/detail")
-    public Response getDetail() throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        Properties properties = new Properties();
-
-        InputStream in = classLoader.getResourceAsStream("git.properties");
-
-        if (Objects.isNull(in)) {
-            return Response.success("获取版本信息失败，请检查jar包文件");
-        }
-
-        properties.load(in);
-
+    public Response getDetail() {
         GitDetail gitDetail = new GitDetail();
-        gitDetail.setBranch(properties.get("git.branch").toString());
-        gitDetail.setCommitMessage(properties.get("git.commit.message.full").toString());
-        gitDetail.setCommitTime(properties.get("git.commit.time").toString());
-
+        try {
+            // 加载git.properties文件，并获取指定属性值
+            Properties properties = loadGitProperties();
+            gitDetail.setBranch(getStringProperty(properties, "git.branch"));
+            gitDetail.setCommitMessage(getStringProperty(properties, "git.commit.message.full"));
+            gitDetail.setCommitTime(getStringProperty(properties, "git.commit.time"));
+        } catch (Exception e) {
+            // 加载出错则返回错误信息
+            return Response.success(e.getMessage());
+        }
         return Response.success(gitDetail);
     }
+
+    /**
+     * 加载git.properties文件
+     *
+     * @return Properties对象
+     * @throws RuntimeException 若读取失败则抛出运行时异常
+     */
+    private Properties loadGitProperties() {
+        Properties properties = new Properties();
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream("git.properties")) {
+            if (Objects.isNull(in)) {
+                log.error("未找到 git.properties 文件");
+                return new Properties();
+            }
+            properties.load(in);
+        } catch (IOException e) {
+            log.error("读取 git.properties 文件失败：", e);
+        }
+        return properties;
+    }
+
+    /**
+     * 获取git.properties文件中的指定属性值
+     *
+     * @param properties Properties对象
+     * @param key        属性名
+     * @return String 属性值
+     */
+    private static String getStringProperty(Properties properties, String key) {
+        return Optional.ofNullable(properties.getProperty(key, "")).map(Object::toString).orElse("");
+    }
+
 
 }
